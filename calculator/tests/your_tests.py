0a47536213e4f2d327c5f226104344921e227bfd
Alex Lee <leealex@stanford.edu>
2023-05-16 04:00:06 -0700

    added make file and test

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,10 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
-
+# Checks that the program outputs "1" for an input of "2 - 1"
+assert run("2 - 1").output == "1"
+# Checks that the program outputs "2" for an input of "4 / 2"
+assert run("4 / 2").output == "2"
 ###
 
 print("All tests passed!")
